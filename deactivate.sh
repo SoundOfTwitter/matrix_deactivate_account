ec828a56c2999ccfc92bbc10a8a0062bf4ea02a8
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2024-11-12 20:26:07 +0800

    Update deactivate.sh

--- deactivate.sh
+++ deactivate.sh
@@ -22,11 +22,22 @@ if [ "$access_token" != "null" ]; then
   echo "停用用户响应: $deactivate_response"
 
   # 删除用户的账户数据
-  # delete_response=$(curl -s -X DELETE "http://localhost:8008/_synapse/admin/v2/users/$user_to_delete" \
-    # -H "Authorization: Bearer $access_token" \
-    # -H "Content-Type: application/json")
+  # delete_response=$(curl -s -X DELETE "http://localhost:8008/_synapse/admin/v1/users/@$user_to_delete:$matrix_domain" \
+  #   -H "Authorization: Bearer $access_token" \
+  #   -H "Content-Type: application/json")
 
   # echo "删除用户响应: $delete_response"
+
+  # 查看所有用户并提取 displayname 字段
+  response=$(curl -s -X GET "http://localhost:8008/_synapse/admin/v2/users" \
+       -H "Authorization: Bearer $access_token")
+
+  # 打印原始响应以进行调试
+  # echo "原始响应: $response"
+
+  # 使用 jq 提取 displayname 并显示在终端上
+  echo $response | jq -r '.users[] | .displayname'
+
 else
   echo "无法获取访问令牌，请检查管理员账户和密码。"
 fi
